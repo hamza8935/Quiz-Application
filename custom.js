887a2ed04d1c05f495eead2e6ec1f300444dd812
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-10 19:45:42 +0500

    update

--- custom.js
+++ custom.js
@@ -1,7 +1,11 @@
 let main = document.querySelector('.main');
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
-
+let btn3 = document.querySelector('.btn3');
+let main2 = document.querySelector('.main2');
 btn2.addEventListener('click',function () {
     main.classList.add('hide');
+})
+btn3.addEventListener('click',function ( ) {
+    main2.classList.add('hide1');
 })
